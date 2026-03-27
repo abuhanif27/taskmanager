da9a0ec6072fd8dc88608d1fe16f11e82c54cf0c
Abu Hanif <ahanif2500@gmail.com>
2026-03-27 18:58:06 +0600

    Add task list view to display tasks using get_list_or_404

--- task/views.py
+++ task/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
+from . import models
 
 # Create your views here.
+def task_list(request):
+    tasks = get_list_or_404(models.Task)
+    return render(request, 'task/task_list.html', {'tasks': tasks})

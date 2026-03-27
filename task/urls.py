f57288a953ea70eff7f968df256e9f182ba86e06
Abu Hanif <ahanif2500@gmail.com>
2026-03-27 16:20:04 +0600

    Add task editing, deletion, and completion features with corresponding views and templates

--- task/urls.py
+++ task/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.task_list, name='task_list'),
     path('tasks/add/', views.add_task, name='add_task'),
+    path('tasks/edit/<int:task_id>/', views.edit_task, name='edit_task'),
+    path('tasks/toggle/<int:task_id>/', views.task_toggle_complete, name='task_toggle_complete'),
+    path('tasks/delete/<int:task_id>/', views.delete_task, name='delete_task'),
 ]
--- task/views.py
+++ task/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, get_object_or_404
 from django.urls import reverse
 from .forms import TaskForm
 from . import models
+
 
 # Create your views here.
 def task_list(request):
     tasks = models.Task.objects.all().order_by('-created_at')
     return render(request, 'task/task_list.html', {'tasks': tasks})
+
 
 def add_task(request):
     if request.method == 'POST':
@@ -15,3 +17,29 @@ def add_task(request):
             form.save()
             return redirect(reverse('task_list'))
     return render(request, 'task/add_task.html', {'form': TaskForm()})
+
+
+def edit_task(request, task_id):
+    task = get_object_or_404(models.Task, id=task_id)
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('task_list'))
+    return render(request, 'task/edit_task.html', {'form': TaskForm(instance=task), 'task': task})
+
+
+def task_toggle_complete(request, task_id):
+    task = models.Task.objects.get(id=task_id)
+    task.completed = not task.completed
+    task.save()
+    return redirect(reverse('task_list'))
+
+
+def delete_task(request, task_id):
+    task = get_object_or_404(models.Task, id=task_id)
+    if request.method == 'POST':
+        task.delete()
+        return redirect(reverse('task_list'))
+
+    return render(request, 'task/delete_task.html', {'task': task})

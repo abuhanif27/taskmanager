ef0dc816a3ae8c4a91de6f24e3c5ca1ff3e91468
Abu Hanif <ahanif2500@gmail.com>
2026-03-29 04:47:26 +0600

    Add migration to associate tasks with users via ForeignKey

---
+++ task/migrations/0002_task_user.py
@@ -0,0 +1,24 @@
+from django.conf import settings
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('task', '0001_initial'),
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='task',
+            name='user',
+            field=models.ForeignKey(
+                blank=True,
+                null=True,
+                on_delete=models.deletion.CASCADE,
+                related_name='tasks',
+                to=settings.AUTH_USER_MODEL,
+            ),
+        ),
+    ]

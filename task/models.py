68438f0c77f29b22a4dcf087270fd5da2805737d
Abu Hanif <ahanif2500@gmail.com>
2026-03-29 04:19:02 +0600

    Add accounts app with custom user model and related configurations

--- task/models.py
+++ task/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-
+from taskmanager.settings import AUTH_USER_MODEL
 # Create your models here.
 class Task(models.Model):
+    user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='tasks')
     title = models.CharField(max_length=200)
     description = models.TextField()
     completed = models.BooleanField(default=False)

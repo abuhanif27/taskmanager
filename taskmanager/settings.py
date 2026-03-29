728a36772ca11bc09e23140744731fdd9f3c560d
Abu Hanif <ahanif2500@gmail.com>
2026-03-29 14:45:21 +0600

    Refactor error templates to enhance user experience with detailed error messages and consistent styling

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^oidrd$&pq_j7sib(sdlyadx1v2m2^3p-3%t0158^)y$08wj2a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'taskmanager.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
